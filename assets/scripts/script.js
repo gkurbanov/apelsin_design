a86a8edfb1e46945946790d18fac36e35827e4d5
Max <pumpkinmaxpro@gmail.com>
2021-06-12 17:44:43 +0500

    add specification and one more slider

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
     $('.slider-nav').slick({
         slidesToShow: 20,
         slidesToScroll: 1,
-        infinite: true,
         asNavFor: '.slider-for',
         arrows: false,
         dots: false,
@@ -20,3 +19,48 @@ $(document).ready(function(){
         draggable: false
     });
 });
+
+/* Slider For Product Slider */
+
+$(document).ready(function(){
+    $('.article-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        infinite: false,
+        arrows: true,
+        speed: 1000,
+        dots: false
+    });
+});
+
+// Read More Specification
+
+const button = document.querySelector('#btn-specification-readmore');
+const hiddenItems = document.querySelectorAll('.hidden-item');
+let isHidden = true;
+button.addEventListener('click', () => {
+  button.textContent = isHidden
+  ? 'Скрыть характеристики'
+  : 'Развернуть характеристики';
+  
+  isHidden = !isHidden;
+  hiddenItems.forEach(item => item.classList.toggle('hidden'));
+});
+
+// Read More Description
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Развернуть описание";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Скрыть описание";
+      moreText.style.display = "inline";
+    }
+  }
